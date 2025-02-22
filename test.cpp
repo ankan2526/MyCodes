799ff0347a1e29469f40f42d83762560f12347b2
Ankan Mahapatra <ankan2526@gmail.com>
2025-02-22 19:32:06 +0530

    test.cpp passed abc394_e

--- test.cpp
+++ test.cpp
@@ -1,48 +1,96 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-vector<int> solution(vector<int> a, vector<int> b, vector<vector<int>> queries) {
-    map<int, int> store;
-    vector<int> ans;
-
-    for (int num : a) {
-        store[num]++;
-    }
-
-    for (const vector<int> &q : queries) {
-        if (q[0] == 0) {
-            int ind = q[1];
-            int val = q[2];
-            b[ind] += val;
-        } else if (q[0] == 1) {
-            int x = q[1];
-            int count = 0;
-            for (int num : b) {
-                count += store[x - num];
-            }
-            ans.push_back(count);
-        }
-    }
-
-    return ans;
-}
-
+const int INF = 1e18+7;
 
 int main() {
-    vector<int> a = {1, 2, 3};
-    vector<int> b = {1, 4};
-    vector<vector<int>> queries = {
-        {1, 5},
-        {0, 0, 2},
-        {1, 5}
-    };
-
-    vector<int> result = solution(a, b, queries);
-
-    for (int val : result) {
-        cout << val << " ";
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int n;
+    cin >> n;
+    
+    vector<string> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
     }
-
+    
+    vector<vector<int>> ans(n, vector<int>(n, INF));
+    
+    for (int i = 0; i < n; i++) {
+        ans[i][i] = 0;
+    }
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (a[i][j] != '-' && i != j) {
+                ans[i][j] = 1;
+            }
+        }
+    }
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i == j) {
+                ans[i][j] = 0;
+                continue;
+            }
+            
+            vector<vector<bool>> visited(n, vector<bool>(n, false));
+            visited[i][j] = true;
+            
+            queue<tuple<int, int, int>> q;
+            q.push({i, j, 0});
+            bool found = false;
+            
+            while (!q.empty()) {
+                queue<tuple<int, int, int>> nq;
+                
+                while (!q.empty()) {
+                    auto [x, y, l] = q.front();
+                    q.pop();
+                    if(l >= ans[i][j]) continue;
+                    
+                    if (ans[x][y] != INF) {
+                        if (ans[x][y] == -1) continue;
+                        ans[i][j] = min(ans[i][j], l + ans[x][y]);
+                        found = true;
+                        continue;
+                    }
+                    
+                    for (int u = 0; u < n; u++) {
+                        if (u == y && a[x][u] != '-' && !visited[u][y]) {
+                            ans[i][j] = min(l + 1, ans[i][j]);
+                            found = true;
+                            nq.push({x, u, l + 1});
+                        }
+                        for (int v = 0; v < n; v++) {
+                            if (a[x][u] == a[v][y] && a[x][u] != '-' && !visited[u][v]) {
+                                visited[u][v] = true;
+                                if (u == v) {
+                                    ans[i][j] = min(l + 2, ans[i][j]);
+                                    found = true;
+                                }
+                                nq.push({u, v, l + 2});
+                            }
+                        }
+                    }
+                }
+                q = nq;
+            }
+            
+            if (!found) {
+                ans[i][j] = -1;
+            }
+        }
+    }
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << ans[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    
     return 0;
 }
